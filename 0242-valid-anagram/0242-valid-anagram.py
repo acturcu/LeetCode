f9d8331454a7de9ec9a25ada754e1042c7c2982c
Andru <92313471+acturcu@users.noreply.github.com>
2023-08-24 00:38:05 +0300

    Time: 54 ms (78.07%), Space: 16.9 MB (61.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+
+        freq = [0] * 26
+        for letter in s:
+            freq[ord(letter) - 97] += 1
+        
         for letter in t:
-            s = s.replace(letter, "", 1)
-        return not len(s)
+            freq[ord(letter) - 97] -= 1
+        
+        for el in freq:
+            if el:
+                return False
+        
+
+        return True
+
+            
